431655c891c34e7fd615be437a99efd37f17a79e
ngtaiduc110 <ndangkhoi560@gmail.com>
2024-10-24 02:38:53 +0700

    update blog

--- src/routes/blog.route.ts
+++ src/routes/blog.route.ts
@@ -7,7 +7,7 @@ import BlogValidation from "../validations/blog.validation"
 const BlogRoute = express.Router()
 
 BlogRoute.post("/createBlog",
-  authMiddleware([Roles.ROLE_TEACHER]),
+  authMiddleware([Roles.ROLE_STUDENT]),
   BlogValidation.createUpdateBlog,
   BlogController.createBlog
 )
