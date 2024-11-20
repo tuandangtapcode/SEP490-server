ec21ddbfe7e393ad815b78e56fea445c263d97d9
tuanpm <tuanpham081102@gmail.com>
2024-11-21 01:22:01 +0700

    update

--- src/services/openai.service.ts
+++ src/services/openai.service.ts
@@ -1,32 +1,39 @@
-import { OpenAI } from "openai";
-import dotenv from "dotenv";
-dotenv.config();
+import { OpenAI } from "openai"
+import dotenv from "dotenv"
+dotenv.config()
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY!,
-});
+  apiKey: process.env.OPENAI_API_KEY!,
+})
 
-export const generateEmbedding = async (text: string): Promise<number[]> => {
-    const response = await openai.embeddings.create({
-        model: "text-embedding-3-large", // The model name you want to use
-        input: text,
-        encoding_format: "float", // Ensure compatibility with Pinecone
-    });
+const generateEmbedding = async (text: string): Promise<number[]> => {
+  const response = await openai.embeddings.create({
+    model: "text-embedding-3-large", // The model name you want to use
+    input: text,
+    encoding_format: "float", // Ensure compatibility with Pinecone
+  })
 
-    // Assuming response.data contains the embeddings
-    return response.data[0].embedding;
-};
+  // Assuming response.data contains the embeddings
+  return response.data[0].embedding
+}
 
-export const getRecommendation = async (prompt: string) => {
-    const response = await openai.chat.completions.create({
-      model: "gpt-4o-mini",
-      messages: [
-        { role: "system", content: "You are a helpful assistant recommending teacher in system to users." },
-        { role: "user", content: prompt },
-      ],
-      max_tokens: 300, // Adjust as needed
-      temperature: 0.7, // Adjust creativity level
-    });
-  
-    return response.choices[0]?.message?.content?.trim() ;
-};
+const getRecommendation = async (prompt: string) => {
+  const response = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      { role: "system", content: "You are a helpful assistant recommending teacher in system to users." },
+      { role: "user", content: prompt },
+    ],
+    max_tokens: 300, // Adjust as needed
+    temperature: 0.7, // Adjust creativity level
+  })
+
+  return response.choices[0]?.message?.content?.trim()
+}
+
+const OpenaiService = {
+  generateEmbedding,
+  getRecommendation
+}
+
+export default OpenaiService
